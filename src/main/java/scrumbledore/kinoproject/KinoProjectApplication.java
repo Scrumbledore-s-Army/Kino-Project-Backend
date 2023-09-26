316417b546d30fcc9ba3df92d43296f7fa355723
simonj1704 <simo915m@stud.kea.dk>
2023-09-26 11:06:04 +0200

    First commit

---
+++ src/main/java/scrumbledore/kinoproject/KinoProjectApplication.java
@@ -0,0 +1,13 @@
+package scrumbledore.kinoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/scrumbledore/kinoproject/KinoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package scrumbledore.kinoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
