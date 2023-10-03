79d72c036a669fc6664699166a2081ec273e1840
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-10-03 14:13:41 +0200

    Added getallfilms and security for endpoint

--- src/main/java/scrumbledore/kinoproject/project/film/api/FilmController.java
+++ src/main/java/scrumbledore/kinoproject/project/film/api/FilmController.java
@@ -5,12 +5,12 @@ package scrumbledore.kinoproject.project.film.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import scrumbledore.kinoproject.project.film.dto.FilmResponse;
 import scrumbledore.kinoproject.project.film.entity.Film;
 import scrumbledore.kinoproject.project.film.service.FilmService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/films")
@@ -21,12 +21,15 @@ public class FilmController {
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
+    @GetMapping
+    public List<FilmResponse> getAllFilms(){
+    return filmService.getAllFilms();
+    }
 
     @RequestMapping("/imdbid/{imdbId}")
     public Film getFilm(@PathVariable String imdbId) {
         return filmService.getFilmByImdbId(imdbId);
     }
-    @PreAuthorize("permitAll()")
     @PostMapping("/{imdbId}")
     public Film addfilm(@PathVariable String imdbId) throws JsonProcessingException {
         return filmService.addFilm(imdbId);
