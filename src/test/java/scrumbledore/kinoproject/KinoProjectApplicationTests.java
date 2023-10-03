fd4eae79ca3fe7614bac9700fd3598d7321e5c5e
simonj1704 <simo915m@stud.kea.dk>
2023-10-03 20:41:01 +0200

    Tests are now passing with using beforeAll setting temp env vars

--- src/test/java/scrumbledore/kinoproject/KinoProjectApplicationTests.java
+++ src/test/java/scrumbledore/kinoproject/KinoProjectApplicationTests.java
@@ -1,10 +1,27 @@
 package scrumbledore.kinoproject;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import scrumbledore.kinoproject.security.TestUtils;
 
 @SpringBootTest
 class KinoProjectApplicationTests {
+
+    @BeforeAll
+    public static void iniSetUp(){
+        System.setProperty(TestUtils.h2UrlName, TestUtils.h2UrlValue);
+        System.setProperty(TestUtils.h2UsernameName, TestUtils.h2UsernameValue);
+        System.setProperty(TestUtils.h2PassName, TestUtils.h2PassValue);
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        System.clearProperty(TestUtils.h2UrlName);
+        System.clearProperty(TestUtils.h2UsernameName);
+        System.clearProperty(TestUtils.h2PassName);
+    }
 
     @Test
     void contextLoads() {
