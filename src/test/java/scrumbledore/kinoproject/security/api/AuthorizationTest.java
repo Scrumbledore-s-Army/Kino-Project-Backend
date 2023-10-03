e0e8ac1f191f777c4cae7df19c38f1036e825cfd
simonj1704 <simo915m@stud.kea.dk>
2023-10-03 16:27:34 +0200

    Tried fixing stuff

--- src/test/java/scrumbledore/kinoproject/security/api/AuthorizationTest.java
+++ src/test/java/scrumbledore/kinoproject/security/api/AuthorizationTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
