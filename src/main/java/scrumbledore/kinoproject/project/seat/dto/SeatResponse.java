9787fc017ec99f0db045f1d305e75f0dccbd85f6
simonj1704 <simo915m@stud.kea.dk>
2023-10-05 11:20:34 +0200

    Added DTO classes

---
+++ src/main/java/scrumbledore/kinoproject/project/seat/dto/SeatResponse.java
@@ -0,0 +1,12 @@
+package scrumbledore.kinoproject.project.seat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SeatResponse {
+
+}
---
+++ src/main/java/scrumbledore/kinoproject/project/theater/dto/TheaterRequest.java
@@ -0,0 +1,11 @@
+package scrumbledore.kinoproject.project.theater.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TheaterRequest {
+}
