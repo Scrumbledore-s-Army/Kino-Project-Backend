16be799798ca6ed4c8ccfe37de3142ad82614e4b
philipbenn <phil658c@stud.kea.dk>
2023-10-10 11:11:08 +0200

    show reservations, service, controller and repo methods

--- src/main/java/scrumbledore/kinoproject/project/reservation/api/ReservationController.java
+++ src/main/java/scrumbledore/kinoproject/project/reservation/api/ReservationController.java
@@ -1,7 +1,32 @@
 package scrumbledore.kinoproject.project.reservation.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import scrumbledore.kinoproject.project.reservation.dto.ReservationResponse;
+import scrumbledore.kinoproject.project.reservation.service.ReservationService;
+import scrumbledore.kinoproject.project.user.dto.UserResponse;
+import scrumbledore.kinoproject.project.user.service.UserService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/reservations")
 public class ReservationController {
+
+    ReservationService reservationService;
+    UserService userService;
+
+    public ReservationController(ReservationService reservationService, UserService userService) {
+        this.reservationService = reservationService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/reservations/{username}")
+    public List<ReservationResponse> getAllReservationsByUser(@PathVariable("username") String username) {
+        UserResponse user = userService.findById(username);
+        return reservationService.getAllReservationsByUser(user);
+    }
+
 }
--- src/main/java/scrumbledore/kinoproject/project/reservation/service/ReservationService.java
+++ src/main/java/scrumbledore/kinoproject/project/reservation/service/ReservationService.java
@@ -1,7 +1,18 @@
 package scrumbledore.kinoproject.project.reservation.service;
 
 import org.springframework.stereotype.Service;
+import scrumbledore.kinoproject.project.reservation.dto.ReservationResponse;
+import scrumbledore.kinoproject.project.reservation.entity.Reservation;
+import scrumbledore.kinoproject.project.reservation.repository.ReservationRepository;
+import scrumbledore.kinoproject.project.user.dto.UserResponse;
+
+import java.util.List;
 
 @Service
 public class ReservationService {
+    ReservationRepository reservationRepository;
+    public List<ReservationResponse> getAllReservationsByUser(UserResponse user) {
+        return reservationRepository.findReservationsByCustomer(user);
+    }
+
 }
